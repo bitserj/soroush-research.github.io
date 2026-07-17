8c2e9e7fab238acf0653c579e7191d64d1b71566
bitserj <nuclearsyndrome@gmail.com>
2026-07-18 02:09:30 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,25 +1,25 @@
 window.APP_CONFIG = {
   appTitle: "سامانه رساله دکتری",
   appSubtitle: "داشبورد پیشرفت پژوهش",
-  researcherName: "نام شما",
+  researcherName: "سروش",
   thesisTitle: "عنوان کامل رساله دکتری",
   chapterTwoTarget: 15000,
   chapterThreeTarget: 8000,
   alertDays: 7,
 
   links: {
-    googleSheet: "https://docs.google.com/spreadsheets/d/1DwnB1iZFr1KZeg88pc4TFMSGV5ROkImfsRX06pEnnaI/edit?pli=1&gid=6886393#gid=6886393",
+    googleSheet: "https://docs.google.com/spreadsheets/d/1Se3wwwJrmtqvrUXVWIKJRHYPFOn29tWL/edit?gid=1911410493#gid=1911410493",
     googleDriveFolder: "https://drive.google.com/drive/folders/1eqqJQo0VxklgdcvUGE4BHcJ1rcn94eop?usp=drive_link"
   },
 
   csv: {
-    annual: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=1249537975&single=true&output=csv",
-    weekly: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=548751474&single=true&output=csv",
-    sections: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=967668392&single=true&output=csv",
-    sources: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=977653018&single=true&output=csv",
-    files: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=6886393&single=true&output=csv",
-    calendar: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=1381170568&single=true&output=csv",
-    history: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=344317764&single=true&output=csv",
-    versions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRFio-W4GioCvK2y0eVkwhffsnpe1kbNyHlrS29nycRWwTmn8dbUFiBFAwsGh4fog_Mm7HxplvydD32/pub?gid=537747906&single=true&output=csv"
+    annual: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1911410493&single=true&output=csv",
+    weekly: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1588974671&single=true&output=csv",
+    sections: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1543797507&single=true&output=csv",
+    sources: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=2016571387&single=true&output=csv",
+    files: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=859835281&single=true&output=csv",
+    calendar: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=2095283486&single=true&output=csv",
+    history: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=760449719&single=true&output=csv",
+    versions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=172035948&single=true&output=csv"
   }
 }
