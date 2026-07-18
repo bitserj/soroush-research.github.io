0cc7e2ae5f5ab0226aecb994d65a690d220205e0
bitserj <nuclearsyndrome@gmail.com>
2026-07-18 10:01:56 +0330

    Update config.js

--- config.js
+++ config.js
@@ -8,18 +8,18 @@ window.APP_CONFIG = {
   alertDays: 7,
 
   links: {
-    googleSheet: "https://docs.google.com/spreadsheets/d/1Se3wwwJrmtqvrUXVWIKJRHYPFOn29tWL/edit?gid=1911410493#gid=1911410493",
+    googleSheet: "https://docs.google.com/spreadsheets/d/1Smi9Nnut7Oo0Ty4bfuMxD7LeLmfYIE7CaX97QICSvmk/edit?gid=1911410493#gid=1911410493",
     googleDriveFolder: "https://drive.google.com/drive/folders/1eqqJQo0VxklgdcvUGE4BHcJ1rcn94eop?usp=drive_link"
   },
 
   csv: {
-    annual: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1911410493&single=true&output=csv",
-    weekly: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1588974671&single=true&output=csv",
-    sections: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=1543797507&single=true&output=csv",
-    sources: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=2016571387&single=true&output=csv",
-    files: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=859835281&single=true&output=csv",
-    calendar: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=2095283486&single=true&output=csv",
-    history: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=760449719&single=true&output=csv",
-    versions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vRK0_n0UZfztAxT-x1RgRb0yWLH7uLRx_Vz8aI0g7vkNhWEl6QmlQ-BArl-hx_AAg/pub?gid=172035948&single=true&output=csv"
+    annual: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=1911410493&single=true&output=csv",
+    weekly: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=1588974671&single=true&output=csv",
+    sections: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=1543797507&single=true&output=csv",
+    sources: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=2016571387&single=true&output=csv",
+    files: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=859835281&single=true&output=csv",
+    calendar: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=2095283486&single=true&output=csv",
+    history: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=760449719&single=true&output=csv",
+    versions: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSZaYnUgWoLUmDiZNHaLm38UK0XS5D-lifJu9bFkrTxWlibXR5gjMIBmSA82WyD0UFHD2JwBuh2Aow7/pub?gid=172035948&single=true&output=csv"
   }
 }
